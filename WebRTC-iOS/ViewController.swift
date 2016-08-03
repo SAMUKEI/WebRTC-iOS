114217d98a8ec841184b00acafcc2cbc58bb0817
Yuta Fujita <yuta.fujita.dvs@diverse-inc.com>
2016-08-03 10:57:02 +0900

    fix metadata nil で crash していたので修正

--- WebRTC-iOS/ViewController.swift
+++ WebRTC-iOS/ViewController.swift
@@ -3,32 +3,32 @@ import AVFoundation
 import UIKit
 
 // Padding space for local video view with its parent.
-let kLocalViewPadding:CGFloat = 20
+let kLocalViewPadding: CGFloat = 20
 
 
-class ViewController : UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet var roomInput:UITextField!
-    @IBOutlet var idField:UITextField!
+    @IBOutlet var roomInput: UITextField!
+    @IBOutlet var idField: UITextField!
 
-    private var peer:Peer?
-    private var isInitiater:Bool = false
+    private var peer: Peer?
+    private var isInitiater: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.roomInput.delegate = self
         self.roomInput.becomeFirstResponder()
-        
+
         self.isInitiater = true
     }
 
-    override func viewDidAppear(animate:Bool) {
+    override func viewDidAppear(animate: Bool) {
         super.viewDidAppear(animate)
-        
+
         if !self.isInitiater { return }
         self.peer?.disconnect()
-        
+
         // full configurations.
         /*
          NSDictionary *config = @{@"host": @"0.peerjs.com",
@@ -42,7 +42,7 @@ class ViewController : UIViewController, UITextFieldDelegate {
          ]
          }};
          */
-        
+
         let config = ["host": "yf-sunaba.tk", "path": "/", "key": "peerjs", "port": 9000, "secure": true,
                       "config":
                         [ "iceServers":
@@ -53,95 +53,95 @@ class ViewController : UIViewController, UITextFieldDelegate {
         guard let peer = self.peer else {
             return
         }
-        peer.onOpen = { (id:String!) in
+        peer.onOpen = { (id: String!) in
             NSLog("onOpen")
             self.idField.text = id
         }
-        
-        peer.onCall = { (sdp:RTCSessionDescription!, metadata: [NSObject: AnyObject]?) in
+
+        peer.onCall = { (sdp: RTCSessionDescription!, metadata: [NSObject: AnyObject]?) in
             NSLog("onCall")
             self.peerClient(self.peer, didReceiveOfferWithSdp:sdp)
         }
-        
-        peer.onError = { (error:NSError!) in
+
+        peer.onError = { (error: NSError!) in
             NSLog("onError: %@", error)
             self.peerClient(self.peer, didError:error)
         }
-        
+
         peer.onClose = { () in
             NSLog("onClose")
             self.resetUI()
         }
-        
+
         peer.start({ () in
         })
     }
-    
-    func applicationWillResignActive(application:UIApplication!) {
+
+    func applicationWillResignActive(application: UIApplication!) {
         self.disconnect()
     }
-    
+
     // MARK: - ARDAppClientDelegate
-    
-    func peerClient(client:Peer!, didReceiveOfferWithSdp sdp:RTCSessionDescription!) {
+
+    func peerClient(client: Peer!, didReceiveOfferWithSdp sdp: RTCSessionDescription!) {
         NSLog("setup CaptureSession!")
         self.isInitiater = false
         self.roomInput.resignFirstResponder()
         self.roomInput.hidden = true
-        
+
         client.answerWithSdp(sdp)
     }
 
-    func peerClient(client:Peer!, didError error:NSError!) {
+    func peerClient(client: Peer!, didError error: NSError!) {
         self.showAlertWithMessage(String(format:"%@", error))
         self.disconnect()
     }
 
     // MARK: - UITextFieldDelegate
-    
-    func textFieldDidEndEditing(textField:UITextField) {
-        let dstId:String! = textField.text
-        
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        let dstId: String! = textField.text
+
         if dstId.characters.count == 0 {
             return
         }
-        
+
         self.isInitiater = true
         textField.hidden = true
-        self.peer?.callWithId(dstId, metadata: nil)
+        self.peer?.callWithId(dstId, metadata: ["" : ""])
     }
-    
-    func textFieldShouldReturn(textField:UITextField) -> Bool {
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
         // There is no other control that can take focus, so manually resign focus
         // when return (Join) is pressed to trigger |textFieldDidEndEditing|.
         textField.resignFirstResponder()
         return true
     }
-    
+
     // MARK: - Private
-    
+
     func disconnect() {
         self.resetUI()
         self.peer?.disconnect()
     }
-    
-    func showAlertWithMessage(message:String!) {
-        let alertView:UIAlertView! = UIAlertView(title:"",
+
+    func showAlertWithMessage(message: String!) {
+        let alertView: UIAlertView! = UIAlertView(title:"",
                                                  message:message,
                                                  delegate:nil,
                                                  cancelButtonTitle:"OK",
                                                  otherButtonTitles:"")
         alertView.show()
     }
-    
+
     func resetUI() {
         self.roomInput.resignFirstResponder()
         self.roomInput.text = nil
         self.roomInput.hidden = false
     }
-    
-    
-    @IBAction func finishCall(sender:AnyObject!) {
+
+
+    @IBAction func finishCall(sender: AnyObject!) {
         self.disconnect()
     }
 }
